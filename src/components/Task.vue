8b266cc2c2e7f2291ad6f2794f1849bb318814ac
Ls7sq <lsq9388@gmail.com>
2021-09-14 12:55:30 +0800

    Vue js List & child components
    the basic of list and child components for vue js

---
+++ src/components/Task.vue
@@ -0,0 +1,18 @@
+<template>
+	<li> 
+	 <div><slot></slot>{{index_out}}</div> 
+	 <small>{{task_out.dueDate}} @ {{task_out.dueTime}}</small>
+	 <button @click="deleteTask(index_out)">delete</button>
+	</li>
+</template>
+
+<script type="">
+	export default{
+		props:['task_out','index_out']
+	}
+	
+</script>
+
+<style type="text/css">
+	
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,100 +1,49 @@
 <template>
   <q-page padding>
-    <button style="position: absolute; right: 10px;"
-    @click=counter++>
-      {{counter}}  
-    </button>
-
-    <input 
-      v-model="message"
-      v-autofocus
-      @keyup.esc="clearMessage"
-      @keyup.enter="aleartMessage"
-      :style="errorStyle"
-      />
-
-    <div>{{message.length}}</div>
-
-    <button @click="clearMessage">clear</button>
-    <h5 
-      v-if="message.length"
-      class="border-gray" >{{message}}</h5>
-
-    <h6 
-      v-else>No Message entered</h6>
-
-      <hr>
-
-      <p>Uppercase message: {{messageUppercase}}</p>
-      <p>Lowercase message: {{message | messageLowercase}}</p>
+    <ul>
+      <task v-for = "(task, index) in tasks"
+      :key='task.id'
+      :task_out='task'
+      :index_out='index'>{{task.name}} </task>
+    </ul>
   </q-page>
 </template>
 
 <script>
 export default {
-
   data(){
-    return{
-      message:"check check",
-      counter: 0
+    return {
+      tasks: [
+        {
+          id:1,
+          name:'Go to shop',
+          dueDate:'2021/09/14',
+          dueTime:'12:00'
+        },
+        {
+          id:2,
+          name:'Get bananas',
+          dueDate:'2021/09/15',
+          dueTime:'14:00'
+        },
+        {
+          id:3,
+          name:'Get apples',
+          dueDate:'2021/09/16',
+          dueTime:'16:00'
+        },
+      ]
     }
   },
-  computed:{
-    messageUppercase(){
-      console.log('message.length: '+ this.message.length);
-      return this.message.toUpperCase() + this.counter;
-    },
-    errorStyle(){
-      if (this.message.length>20 || this.message === "luoshiqi") {
-        return{
-          'color':'red',
-          'background-color':'pink'
-        }
-      }
+  methods:{
+    deleteTask(index){
+      this.tasks.splice(index,1)
     }
   },
-  methods: {  
-    clearMessage(){
-      this.message = '';
-    },
-    // handleKeyup(e){
-    //   console.log(e)
-    //   if (e.keyCode === 27) {
-    //     this.clearMessage()
-    //   }else if(e.keyCode === 13){
-    //     this.aleartMessage()
-    //   }
-    // },
-    aleartMessage(){
-      alert(this.message);
-    }
-  },
-  filters:{
-    messageLowercase(value){
-      //console.log('messageLowercase was fired');//After button click still be active
-      return value.toLowerCase();
-    }
-  },
-  directives:{
-    autofocus:{
-      inserted(el){
-        //console.log('input insterted')
-        el.focus();
-      }
-    }
+  components:{
+    'task':require('components/Task.vue').default
   }
 }
 </script>
 
-<style type="text/css">
-  .border-gray{
-    border:  1px solid grey;
-  }
-  input , button{
-    font-size: 24px;
-  }
-  .error{
-    color: red;
-    background-color: pink;
-  }
 </style>
